ace0e36a551bda1f36538f0f5a8daf4204c07ed7
Floemia <mcescobar73@gmail.com>
2024-11-15 13:59:16 -0300

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { droid } from "./functions";
-
+import type { DroidMods, DroidUser, DroidScore, DroidScoreParameters, DroidUserParameters } from "../typings/index"
 
 export { droid }
+export type { DroidMods, DroidScore, DroidUser, DroidScoreParameters, DroidUserParameters }
+
 
 export default {
-	droid
+	droid,
 }
